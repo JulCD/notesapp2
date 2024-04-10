21a4ac5ae183952967e567e2b23f961d490a338b
Julia <143531581+JulCD@users.noreply.github.com>
2024-04-10 14:54:16 -0500

    Create note function and ui working!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useReducer } from 'react';
 import { generateClient } from 'aws-amplify/api';
-import { List } from 'antd';
+import { List, Input, Button } from 'antd';
 import 'antd/dist/reset.css';
 import { listNotes } from './graphql/queries';
+import { v4 as uuid } from 'uuid';
+import { createNote as CreateNote } from './graphql/mutations';
+
+const CLIENT_ID = uuid();
 
 const initialState = {
   notes: [],
@@ -17,6 +20,16 @@ function reducer(state, action) {
   switch (action.type) {
     case 'SET_NOTES':
       return { ...state, notes: action.notes, loading: false };
+
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes] };
+
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form };
+
+    case 'SET_INPUT':
+      return { ...state, form: { ...state.form, [action.name]: action.value } };
+
     case 'ERROR':
       return { ...state, loading: false, error: true };
     default:
@@ -40,6 +53,31 @@ const App = () => {
       console.error(err);
       dispatch({ type: 'ERROR' });
     }
+  };
+
+  const createNote = async () => {
+    const { form } = state //destructuring getting the form element out of the state
+
+    if (!form.name || !form.description) {
+      return alert('please enter a name and a description')
+    }
+
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() }
+    dispatch({ type: 'ADD_NOTE', note })
+    dispatch({ type: 'RESET_FORM' })
+    try {
+      await client.graphql({
+        query: CreateNote,
+        variables: { input: note }
+      })
+      console.log('successfully created note!')
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const onChange = (e) => {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value });
   };
 
   useEffect(() => {
@@ -66,6 +104,26 @@ const App = () => {
 
   return (
     <div style={styles.container}>
+
+      <Input
+        onChange={onChange}
+        value={state.form.name}
+        placeholder='Note Name'
+        name='name'
+        style={styles.input}
+      />
+      <Input
+        onChange={onChange}
+        value={state.form.description}
+        placeholder='Note Description'
+        name='description'
+        style={styles.input}
+      />
+      <Button
+        onClick={createNote}
+        type='primary'
+      >Create Note</Button>
+
       <List
         loading={state.loading}
         dataSource={state.notes}
