20ab455d06d8dc96cfd89deab71e8d9680652eda
Julia <143531581+JulCD@users.noreply.github.com>
2024-04-10 14:16:23 -0500

    lots of amplify stuff
    amplify init
    amplify add api
    amplify push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "notesapptwoapi": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
